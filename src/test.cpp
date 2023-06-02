823779f97223e05a598cd646ae628a81c08b66f8
mpadge <mark.padgham@email.com>
2023-06-02 11:45:41 +0200

    simplify src to minimal reprex

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,27 @@
-#include "test.h"
+#include <set>
+#include <Rcpp.h>
+
+struct TestStruct
+{
+    TestStruct (double a): _a (a) {}
+    
+    double _a;
+};
+
+struct by_a
+{
+    bool operator () (const TestStruct& lhs, const TestStruct& rhs)
+    {
+        return lhs._a < rhs._a;
+    }
+}; 
+
+typedef std::set <TestStruct, by_a> TestSet;
 
 TestSet Set_function ()
 {
     TestSet test_set;
-
-    test_set.insert (TestStruct (0.0, 0.0));
+    test_set.insert (TestStruct (0.0));
 
     return test_set;
 }
@@ -15,7 +32,5 @@ TestSet Set_function ()
 // [[Rcpp::export]]
 Rcpp::IntegerVector rcpp_test ()
 {
-    TestSet test_set = Set_function ();
-
     return 0L;
 }
